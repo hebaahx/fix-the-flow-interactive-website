0f1945e7582a259ceec3acbf3d902457f3ba58a8
handouh1 <heba.handouka@hva.nl>
2025-12-04 00:51:03 +0100

    #2 view password js toegevoegd
    in stappen js toegevoegd

--- password.js
+++ password.js
@@ -1,7 +1,18 @@
-// selecteer button die je wilt veranderen
+// stap 1 - document.querySelector
+// zoek en selecteer de button die je wilt veranderen
+const viewPassword =
+document.querySelector('.toggle-btn')
 
-
+// stap 2 - addEventListener
 // wachten tot er op wordt geklikt
+viewPassword.addEventListener('click', toggleviewPassword)
 
-
+// stap 3 - functie en toggle
+// ook voor het inputveld herhalen, zodat die ook mee veranderd
 // password zichtbaar maken
+function toggleviewPassword() {
+    const passwordField = 
+    document.querySelector('#current-password')
+    passwordField.type = passwordField.type == 'password' ? 'text' : 'password';
+    viewPassword.classList.toggle("view")   //voor styling van button
+}
