a11bb24ae8e5074e484194b21f893ae24933db86
handouh1 <heba.handouka@hva.nl>
2025-12-10 23:38:59 +0100

    #12 #2 svg change
    SVG veranderd van open naar closed eye

--- password.js
+++ password.js
@@ -5,14 +5,27 @@ document.querySelector('.toggle-btn')
 
 // stap 2 - addEventListener
 // wachten tot er op wordt geklikt
-viewPassword.addEventListener('click', toggleviewPassword)
+viewPassword.addEventListener('click', function() {
 
-// stap 3 - functie en toggle
-// ook voor het inputveld herhalen, zodat die ook mee veranderd
-// password zichtbaar maken
-function toggleviewPassword() {
-    const passwordField = 
-    document.querySelector('#current-password')
-    passwordField.type = passwordField.type == 'password' ? 'text' : 'password';
-    viewPassword.classList.toggle("view")   //voor styling van button
+// stap 3 - selecteer inputveld en svg
+const passwordField = document.querySelector('#current-password')
+const icon = viewPassword.querySelector('img')
+
+//check of het veld nu password is
+if(passwordField.type == 'password') { // als het veld password is
+    //maak tekst zichtbaar
+    passwordField.type = 'text'
+    //verander svg naar oog met streepje er doorheen
+    icon.src = 'assets/hideeye.svg' 
 }
+//als het niet zo is
+else {
+    //maak tekst weer verborgen
+    passwordField.type = 'password'
+    //verander svg naar open oog
+    icon.src = 'assets/vieweye.svg'
+}
+
+}
+)
+
